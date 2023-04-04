62002e05632d26eef734a130c4717e0fbb4cdc8b
S-furi <stefano.furi@studio.unibo.it>
2023-04-04 12:59:58 +0200

    Fixed depencencies

---
+++ kt/prjs/broadcasting/src/test/kotlin/ndArrayBroadcasting/NDArrayBroadcastingKtTest.kt
@@ -0,0 +1,22 @@
+package ndArrayBroadcasting
+
+import org.junit.jupiter.api.AfterEach
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+
+import org.junit.jupiter.api.Assertions.*
+
+class NDArrayBroadcastingKtTest {
+
+    @Test
+    fun findDimToBroadcast() {
+    }
+
+    @Test
+    fun broadcastAdd() {
+    }
+
+    @Test
+    fun get2DBroadcastingMatrix() {
+    }
+}
--- kt/prjs/dfExtensions/build.gradle.kts
+++ kt/prjs/dfExtensions/build.gradle.kts
@@ -8,12 +8,23 @@ version = "1.0-SNAPSHOT"
 
 repositories {
     mavenCentral()
+    maven("https://repo.kotlin.link")
 }
 
 dependencies {
     testImplementation(kotlin("test"))
+<<<<<<< HEAD:kt/prjs/dfExtensions/build.gradle.kts
     implementation("org.jetbrains.kotlinx:dataframe:0.9.1")
+=======
+    implementation("org.jetbrains.kotlinx:multik-core:0.2.1")
+    implementation("org.jetbrains.kotlinx:multik-default:0.2.1")
+    implementation("space.kscience:kmath-core:0.3.0")
+    implementation("space.kscience:kmath-tensors:0.3.0")
+    implementation("space.kscience:kmath-multik:0.3.0")
+    implementation("space.kscience:kmath-stat:0.3.0")
+>>>>>>> c57cd56 (Fixed depencencies):kt/prjs/broadcasting/build.gradle.kts
 }
+
 
 tasks.test {
     useJUnitPlatform()
