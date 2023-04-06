c6362d55625a8c321a0e31db66dd8e85152a7ed1
S-furi <stefano.furi@studio.unibo.it>
2023-04-06 10:29:27 +0200

    Fixed git conflict on configuration file

--- kt/intellij/dfExtensions/build.gradle.kts
+++ kt/intellij/dfExtensions/build.gradle.kts
@@ -3,26 +3,18 @@ plugins {
     application
 }
 
-group = "org.example"
-version = "1.0-SNAPSHOT"
-
 repositories {
     mavenCentral()
-    maven("https://repo.kotlin.link")
 }
 
 dependencies {
     testImplementation(kotlin("test"))
-<<<<<<< HEAD:kt/prjs/dfExtensions/build.gradle.kts
+
+    // Dataframe Dependency
     implementation("org.jetbrains.kotlinx:dataframe:0.9.1")
-=======
+    // Multik Dependencies
     implementation("org.jetbrains.kotlinx:multik-core:0.2.1")
     implementation("org.jetbrains.kotlinx:multik-default:0.2.1")
-    implementation("space.kscience:kmath-core:0.3.0")
-    implementation("space.kscience:kmath-tensors:0.3.0")
-    implementation("space.kscience:kmath-multik:0.3.0")
-    implementation("space.kscience:kmath-stat:0.3.0")
->>>>>>> c57cd56 (Fixed depencencies):kt/prjs/broadcasting/build.gradle.kts
 }
 
 
