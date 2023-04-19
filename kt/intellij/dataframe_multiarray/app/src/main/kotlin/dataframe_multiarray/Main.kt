89a5a46271506c2047bd19d7e00d43bc80743570
S-furi <stefano.furi@studio.unibo.it>
2023-04-19 10:31:03 +0200

    rebased multi_array from main

---
+++ kt/intellij/dataframe_multiarray/app/src/main/kotlin/dataframe_multiarray/Main.kt
@@ -0,0 +1,4 @@
+package dataframe_multiarray
+
+fun main() {
+}
---
+++ kt/intellij/dataframe_multiarray/app/src/test/kotlin/dataframe_multiarray/DFMultikTests.kt
@@ -0,0 +1,35 @@
+package dataframe_multiarray
+
+import org.jetbrains.kotlinx.dataframe.api.columnOf
+import org.jetbrains.kotlinx.dataframe.api.dataFrameOf
+import org.jetbrains.kotlinx.multik.api.arange
+import org.jetbrains.kotlinx.multik.api.mk
+import org.jetbrains.kotlinx.multik.ndarray.data.D1
+import org.jetbrains.kotlinx.multik.ndarray.data.D2
+import org.jetbrains.kotlinx.multik.ndarray.data.NDArray
+import org.junit.jupiter.api.Test
+import kotlin.test.assertEquals
+
+class DFMultikTests {
+
+    @Test
+    fun testMultik1DNDarrayToColumn() {
+        val values : NDArray<Double, D1> = mk.arange<Double>(0, 10, 1)
+        val mkCol = values.toColumn()
+        val expected = columnOf(0.0, 1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0, 9.0)
+
+        assertEquals(mkCol, expected)
+    }
+
+    @Test
+    fun testMultik2DNDArrayToDataFrame() {
+        val values: NDArray<Double, D2> = mk.arange<Double>(0, 10, 1).reshape(5, 2)
+        val mkDF = values.toDataFrame()
+        val expected = dataFrameOf(
+            "0" to listOf(0.0, 2.0, 4.0, 6.0, 8.0),
+            "1" to listOf(1.0, 3.0, 5.0, 7.0, 9.0)
+        )
+
+        assertEquals(mkDF, expected)
+    }
+}
